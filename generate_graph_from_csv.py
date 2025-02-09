3f2f56a5ebff1e3b39b7acc9bcae7fbeeef4e5cf
allysson-assuncao <anybody723@proton.me>
2025-02-09 10:27:52 -0300

    final code probably

--- generate_graph_from_csv.py
+++ generate_graph_from_csv.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("results.csv")
+df = pd.read_csv("different_results.csv")
 
 algorithms = df['Algorithm'].unique()
 orders = df['Order'].unique()
 colors = plt.cm.tab10.colors
 line_styles = ['-', '--', ':']
+
+time_scale = [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000, 100000, 1000000]
+
+sizes = [100, 500, 1000, 5000, 20000, 50000, 100000, 500000]
 
 for algorithm in algorithms:
     plt.figure(figsize=(10, 6))
@@ -22,7 +26,16 @@ for algorithm in algorithms:
     plt.title(f'Performance of {algorithm}')
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.grid(True)
+
     plt.xscale('log')
     plt.yscale('log')
+
+    plt.yticks(time_scale, labels=[str(t) for t in time_scale])
+
+    plt.xticks(sizes, labels=[str(s) for s in sizes], rotation=45)
+
+    for size in sizes:
+        plt.axvline(x=size, color='gray', linestyle=':', alpha=0.3)
+
     plt.tight_layout()
     plt.show()
