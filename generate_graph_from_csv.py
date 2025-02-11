6ffa20f3b4220919e298177d49968b0351e81464
allysson-assuncao <anybody723@proton.me>
2025-02-11 08:01:53 -0300

    changing the scale

--- generate_graph_from_csv.py
+++ generate_graph_from_csv.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("different_results.csv")
+df = pd.read_csv("different_results2.csv")
 
 algorithms = df['Algorithm'].unique()
 orders = df['Order'].unique()
 colors = plt.cm.tab10.colors
 line_styles = ['-', '--', ':']
-
-time_scale = [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000, 100000, 1000000]
 
 sizes = [100, 500, 1000, 5000, 20000, 50000, 100000, 500000]
 
@@ -21,16 +19,14 @@ for algorithm in algorithms:
                  label=f'{order}',
                  color=colors[j], linestyle=line_styles[j], marker='o')
 
-    plt.xlabel('Size of Array')
-    plt.ylabel('Average Time (milliseconds)')
-    plt.title(f'Performance of {algorithm}')
+    plt.xlabel('Tamanho do Vetor')
+    plt.ylabel('Tempo médio (ms)')
+    plt.title(f'Desempenho do {algorithm}')
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.grid(True)
 
     plt.xscale('log')
-    plt.yscale('log')
-
-    plt.yticks(time_scale, labels=[str(t) for t in time_scale])
+    plt.yscale('linear')
 
     plt.xticks(sizes, labels=[str(s) for s in sizes], rotation=45)
 
